c05f0f3399a39a43236ca2adcb00320fb507473b
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-06-06 12:00:24 +0200

    Added interfaces for Vector2 and Vector3

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,16 @@
+export interface IVector2 {
+    readonly kEpsilon: number;
+    readonly kEpsilonNormalSqrt: number;
+
+    x: number;
+    y: number;
+}
+
+export interface IVector3 {
+    readonly kEpsilon: number;
+    readonly kEpsilonNormalSqrt: number;
+
+    x: number;
+    y: number;
+    z: number;
+}
