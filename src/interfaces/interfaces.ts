23fe5d874c85bff7b5a74be3f256cfe2e2fdd1ac
Prism | Software Developer <prism27.dev@gmail.com>
2026-06-16 15:20:14 -0400

    📝 docs(interfaces): add JSDoc comments to IVector2, IVector3, and IVector4

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,17 +1,68 @@
 export interface IVector2 {
+    /**
+     * X component of the vector.
+     *
+     * @type {number}
+     */
     x: number;
+
+    /**
+     * Y component of the vector.
+     *
+     * @type {number}
+     */
     y: number;
 }
 
 export interface IVector3 {
+    /**
+     * X component of the vector.
+     *
+     * @type {number}
+     */
     x: number;
+
+    /**
+     * Y component of the vector.
+     *
+     * @type {number}
+     */
     y: number;
+
+    /**
+     * Z component of the vector.
+     *
+     * @type {number}
+     */
     z: number;
 }
 
 export interface IVector4 {
+    /**
+     * X component of the vector.
+     *
+     * @type {number}
+     */
     x: number;
+
+    /**
+     * Y component of the vector.
+     *
+     * @type {number}
+     */
     y: number;
+
+    /**
+     * Z component of the vector.
+     *
+     * @type {number}
+     */
     z: number;
+
+    /**
+     * W component of the vector.
+     *
+     * @type {number}
+     */
     w: number;
 }
